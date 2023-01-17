558e32ef2b97a689077593dd36c34f5e1a621961
slexicon <lexiscott0101@gmail.com>
2023-01-16 22:07:47 -0800

    add tests

---
+++ test/manager.test.js
@@ -0,0 +1,17 @@
+const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+    it("should create an object with an object that includes the information of the object shown below", () => {
+        const name = "Name";
+        const id = "123";
+        const email = "Name@email.com";
+        const role = "Employee";
+
+        const obj = new Employee(name, id, email, role)
+
+        expect(obj.name).toEqual("Name");
+        expect(obj.id).toEqual("123");
+        expect(obj.email).toEqual("Name@email.com");
+        expect(obj.role).toEqual("Employee");
+    });
+})
