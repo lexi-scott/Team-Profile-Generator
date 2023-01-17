ae30a17216dbd89a455fdacf5260d56168bfd4ae
slexicon <lexiscott0101@gmail.com>
2023-01-16 17:54:24 -0800

    add functions to build objects and html

--- index.js
+++ index.js
@@ -1,16 +1,192 @@
+const inquirer = require('inquirer')
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs');
+
+let teamList = []
+
+function buildTeam() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'first',
+                message: 'What would you like to do?',
+                choices: ['Add a manager', 'Add an engineer', 'Add an intern', 'Finish']
+            }
+        ])
+        .then((data) => {
+            if (data.first === "Add a manager") {
+                addManager();
+            }
+
+            else if (data.first === "Add an engineer") {
+                addEngineer();
+            }
+
+            else if (data.first === "Add an intern") {
+                addIntern();
+            }
+
+            else if (data.first === "Finish") {
+                console.log("you are finished")
+                console.log(teamList)
+            };
+        });
+};
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Employee's first and last name:"
+            },
+
+            {
+                type: "input",
+                name: "id",
+                message: "Employee's ID:"
+            },
+
+            {
+                type: "input",
+                name: "role",
+                message: "Employee's role",
+                default: "Engineer"
+            },
+
+            {
+                type: "input",
+                name: "email",
+                message: "Employee's email:"
+            },
+
+            {
+                type: "input",
+                name: "github",
+                message: "Employee's github:"
+            }
+        ])
+        .then((data) => {
+            engineer = new Engineer(data.name, data.id, data.email, data.role, data.github)
+            teamList.push(engineer)
+            buildTeam();
+        });
+}
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Employee's first and last name:"
+            },
+
+            {
+                type: "input",
+                name: "id",
+                message: "Employee's ID:"
+            },
 
 
-generateHTML 
+            {
+                type: "input",
+                name: "role",
+                message: "Employee's role",
+                default: "Intern"
+            },
 
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Team</title>
-</head>
-<body>
-    <header>My Team</header>
-</body>
-</html>`
+            {
+                type: "input",
+                name: "email",
+                message: "Employee's email:"
+            },
+
+            {
+                type: "input",
+                name: "school",
+                message: "Employee's school:"
+            }
+        ])
+        .then((data) => {
+            intern = new Intern(data.name, data.id, data.email, data.role, data.school);
+            teamList.push(intern);
+            buildTeam();
+        });
+}
+
+function addManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Employee's first and last name:"
+            },
+
+            {
+                type: "input",
+                name: "id",
+                message: "Employee's ID:"
+            },
+
+            {
+                type: "input",
+                name: "role",
+                message: "Employee's role",
+                default: "Manager"
+            },
+
+            {
+                type: "input",
+                name: "email",
+                message: "Employee's email:"
+            },
+
+            {
+                type: "input",
+                name: "office",
+                message: "Office number:"
+            }
+        ])
+        .then((data) => {
+            manager = new Manager(data.name, data.id, data.email, data.role, data.office);
+            teamList.push(manager);
+            buildTeam();
+        });
+}
+
+buildTeam();
+
+
+
+//one function to ask user what do you want to do? intern, engineer, manager or finished. use .then to handle the answer, depending on answer run another function
+
+//then run manager function, intern function, engineer function or finished function.
+
+//addManager, addIntern, addEngineer, finished, introQuestion
+
+// const data = [ Manager, Engineer, Intern ] --> [ { name: "tom"}, {name: }, {}]
+
+// Create a new CARD for each DATA OBJECT (EMPLOYEE)
+// for loop to loop through each OBJECT
+// function createManagerCard
+// function createEngineerCard
+
+//const htmlCards = []
+
+
+// Then where does the new card go(?)
+
+
+// generateIndexFile() function -->
+
+// template literals  --> `# ${}    `
+
+// have a function that creates each card
+// template literals  --> 
